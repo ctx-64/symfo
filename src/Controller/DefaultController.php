4849da271511a77570e2565f879923c2a428a5f5
deborah costeux <deborah.costeux@gmail.com>
2019-12-03 17:48:54 +0100

    symfo 3

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+// src/Controller/WildController.php
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+Class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index() : Response
+    {
+        return $this->render('wild/home.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+}
